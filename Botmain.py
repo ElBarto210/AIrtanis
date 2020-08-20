a5be9178adfcacd82aee2af5743c501616bca6c4
Alejandro Bartolome Fernandez <alebarfer@gmail.com>
2020-08-20 14:18:39 +0200

    build workers function

--- Botmain.py
+++ Botmain.py
@@ -1,12 +1,22 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import NEXUS, PROBE, PYLON
 
 
 class Airtanis(sc2.BotAI):
     async def on_step(self, iteration):
         # what to do every step
         await self.distribute_workers()  # in sc2/bot_ai.py
+        await self.build_workers()
+
+    async def build_workers(self):
+        # nexus = command center
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
+
+    
 
 
 run_game(maps.get("AbyssalReefLE"), [
